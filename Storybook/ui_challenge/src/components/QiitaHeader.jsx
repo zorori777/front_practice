cd9056a4584702c8c49ade2a0e5b542769bede63
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-05-19 18:05:26 +0900

    20180519

---
+++ Storybook/ui_challenge/src/components/QiitaHeader.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper =　styled.div `
+  background-color: #55c500
+  color: #fff;
+  height: 56px;
+
+  a:hover {
+    cursor : pointer;
+  }
+
+  .Wrap {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .box {
+    margin-left: 12px;
+  }
+
+  .left-content {
+    width: 50%;
+    display: flex;
+    justify-content: flex-start;
+
+    .left-content__box {
+      margin-left: 12px;
+      display: flex;
+    }
+  }
+
+  .right-content {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+
+    .right-content__box {
+      margin-right: 12px;
+    }
+  }
+`
+
+const QiitaHeader = () => {
+  return (
+    <Wrapper>
+      <header className="Wrap">
+        <div className="left-content">
+          <a className="left-content__box">Qiita</a>
+          <i class="fas fa-caret-down"></i>
+          <div className="left-content__box">
+            <div>ホーム</div>
+            <i class="fas fa-caret-down"></i>
+          </div>
+          <div className="left-content__box">
+            <div>コミュニティ</div>
+            <i class="fas fa-caret-down"></i>
+          </div>
+           {/* 後form componentを作成する予定 */}
+          <div className="left-content__box">Form</div>
+        </div>
+        <div className="right-content">
+          <a className="right-content__box">ストック一覧</a>
+          <a className="right-content__box">投稿する</a>
+          <div className="right-content__box">０</div>
+          <div className="right-content__box">アイコン</div>
+        </div>
+      </header>
+    </Wrapper>
+  )
+}
+
+export default QiitaHeader
--- Storybook/ui_challenge/src/stories/index.js
+++ Storybook/ui_challenge/src/stories/index.js
@@ -5,11 +5,15 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from '../components/Button'
+import QiitaHeader from '../components/QiitaHeader'
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀) 😎😀 👍😎 💯👍</Button>)
 
-  // 今後追加したときの書き方例
-  // storiesOf('WantedlyJobPosting', module)
-  // .add('default', () => <WantedlyJobPosting />);
+// 今後追加したときの書き方例
+// storiesOf('WantedlyJobPosting', module)
+// .add('default', () => <WantedlyJobPosting />);
+
+storiesOf('QiitaHeader', module)
+.add('default', () => <QiitaHeader />);
