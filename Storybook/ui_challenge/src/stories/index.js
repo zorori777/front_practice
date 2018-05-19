d76334b1d09ac539d6818608af40424a023b7b0f
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-05-19 15:35:43 +0900

    StoryBookにstyled-componentを追加

--- Storybook/ui_challenge/src/stories/index.js
+++ Storybook/ui_challenge/src/stories/index.js
@@ -9,3 +9,7 @@ import Button from '../components/Button'
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀) 😎😀 👍😎 💯👍</Button>)
+
+  // 今後追加したときの書き方例
+  // storiesOf('WantedlyJobPosting', module)
+  // .add('default', () => <WantedlyJobPosting />);
