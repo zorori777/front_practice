eb8c948643325ff9d8fb23ee7532a63f3c2eb209
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-05-24 01:20:00 +0900

    Fix code for react-router

--- React/yahoo-shopping-ranking/src/index.js
+++ React/yahoo-shopping-ranking/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
-import * as reducers from './reducers';
+import createStore from './createStore';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-  combineReducers(reducers),
-  applyMiddleware(logger)
-);
+const history = createBrowserHistory()
+const store = createStore(history);
 
 ReactDOM.render(
-  <Provider>
-    <App />
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
